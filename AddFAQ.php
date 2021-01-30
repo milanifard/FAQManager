ca91d009adde0f432c93c845e0fc1281ef45f54a
MasoudK <masoud.kermanipoor@gmail.com>
2021-01-30 19:03:43 +0330

    sample code of add faq

--- AddFAQ.php
+++ AddFAQ.php
@@ -1,2 +1,31 @@
 <?php
-echo("add faq");
+include "header.inc.php";
+
+HTMLBegin();
+
+if (isset($_GET["save"])){
+    echo($_GET["title"]);
+    echo("<br>");
+    echo($_GET["answer"]);
+    echo("<br>");
+    echo($_GET["click_count"]);
+}
+
+?>
+
+    <form method="get">
+        <div>
+             <label for="title">سوال :</label><input type="text" name="title" id="title">
+        </div>
+        <div>
+             <label for="answer">جواب :</label><input type="text" name="answer" id="answer">
+        </div>
+        <div>
+             <label for="click_count">تعداد کلیک :</label><input type="number" name="click_count" id="click_count">
+        </div>
+        <div>
+            <input type="submit" name="save" id="save" value="ذخیره">
+        </div>
+    </form>
+</body>
+</html>
