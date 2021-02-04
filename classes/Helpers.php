d55ccf036f2e501385904d3fc1263c24a140b0c8
jimist <arash.jeem@gmail.com>
2021-02-05 00:47:32 +0330

    custom helpers for page begin and ends
    Signed-off-by: jimist <arash.jeem@gmail.com>

---
+++ classes/Helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+function FAQHTMLBegins($title = 'تیکتینگ')
+{
+    ?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" href="css/faq.css"/>
+
+    </head>
+    <body>
+
+    <?php
+}
+
+function FAQHTMLEnds()
+{
+    ?>
+    </body>
+    </html>
+    <?php
+}
