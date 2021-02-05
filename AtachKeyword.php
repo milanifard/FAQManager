2a7d2c97511952db367a7fac6f13f6985577b4c9
Nafissad <sadeghi.nafise77@gmail.com>
2021-02-05 21:13:00 +0330

    add table to attach keyword

--- AtachKeyword.php
+++ AtachKeyword.php
@@ -2,37 +2,81 @@
 require_once "header.inc.php";
 require_once "classes/FAQ.php";
 require_once "classes/Keyword.php";
-
 require_once "classes/Helpers.php";
 
 FAQHTMLBegins();
 
 $faq = null;
-if (isset($_GET["faq"])){
+if (isset($_GET["faq"])) {
     $faq = FAQ::getById($_GET["faq"]);
 }
 
-if ($faq === null){
+if ($faq === null) {
     return;
 }
+?>
 
+<table class="table table-striped attach-keyword-container">
+    <tr>
+        <td>
+            سوال
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?php echo $faq->title; ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            جواب
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?php echo $faq->answer; ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            تعداد کلیک
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?php echo $faq->click_count; ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            کلمه کلیدی
+        </td>
+    </tr>
+    <tr>
+        <td>
+
+        </td>
+    </tr>
+</table>
+
+<?php
 echo("<div>");
 
 echo("<div>");
-echo("سوال ".$faq->title);
+echo("سوال " . $faq->title);
 echo("</div>");
 
 echo("<div>");
-echo("جواب ".$faq->answer);
+echo("جواب " . $faq->answer);
 echo("</div>");
 
 echo("<div>");
-echo("تعداد کلیک ".$faq->click_count);
+echo("تعداد کلیک " . $faq->click_count);
 echo("</div>");
 
 echo("</div>");
 
-if (isset($_GET["keyword"])){
+if (isset($_GET["keyword"])) {
     Keyword::atacheToFAQ($faq, $_GET["keyword"]);
 }
 
@@ -41,7 +85,7 @@ echo("<div>
 <label for=\"keyword\">کلمه کلیدی :</label><input type=\"text\" name=\"keyword\" id=\"keyword\">
 </div>");
 echo("<div>
-<input type=\"hidden\" name=\"faq\" id=\"faq\" value=\"".$faq->id."\">
+<input type=\"hidden\" name=\"faq\" id=\"faq\" value=\"" . $faq->id . "\">
 </div>");
 echo("<div>
 <input type=\"submit\" name=\"save\" id=\"save\" value=\"ذخیره\">
@@ -52,9 +96,9 @@ echo("</form>");
 echo("<table style=\"width: 100%\">");
 
 $keywords = Keyword::getAllAcceptedByFAQ($faq);
-foreach ($keywords as $k){
+foreach ($keywords as $k) {
     echo("<tr>");
-    echo("<td>".$k->term."</td>");
+    echo("<td>" . $k->term . "</td>");
     echo("</tr>");
 }
 
