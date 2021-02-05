f15b1052cc29a431146b445d61ccb24c74573d53
jimist <arash.jeem@gmail.com>
2021-02-05 20:47:43 +0330

    reformat Add FAQ form
    Signed-off-by: jimist <arash.jeem@gmail.com>

--- AddFAQ.php
+++ AddFAQ.php
@@ -21,55 +21,64 @@ if (isset($_GET["save"])) {
 
 ?>
 
-<form method="get">
-    <div>
-        <label for="title">سوال :</label><input type="text" name="title" id="title">
-    </div>
-    <div>
-        <label for="answer">جواب :</label><input type="text" name="answer" id="answer">
-    </div>
-    <div>
-        <label for="click_count">تعداد کلیک :</label><input type="number" name="click_count" id="click_count">
-    </div>
+    <form method="get" class="add-faq-form col-6 col-md-4">
+        <div class="text-center">
+            <label for="title">سوال :</label>
+            <br>
+            <input type="text" name="title" id="title" class="col-10">
+        </div>
+        <div class="text-center">
+            <label for="answer">جواب :</label>
+            <br>
+            <textarea name="answer" id="answer" rows="5" class="col-10"></textarea>
+        </div>
+        <div class="text-center">
+            <label for="click_count">تعداد کلیک :</label>
+            <br>
+            <input type="number" name="click_count" id="click_count" value="0" class="col-10">
+        </div>
 
-    <div>
+        <div class="add-faq-target-choose">
+            <?php
+            require_once "classes/UserGroup.php";
+
+            $userGroups = UserGroup::getAll();
+            foreach ($userGroups as $u) {
+                echo("<label for=\"" . $u->title . "\">" . $u->persionTitle . "</label><input type=\"checkbox\" id=\"" . $u->title . "\" name=\"" . $u->title . "\" value=\"" . $u->title . "\" checked>");
+            }
+            ?>
+        </div>
+
+        <div>
+            <input type="submit" name="save" class="btn btn-primary" id="save" value="ذخیره">
+        </div>
+    </form>
+
+    <table style="width: 100%" class="table table-striped">
+        <thead>
+        <tr>
+            <th>شناسه</th>
+            <th>سوال</th>
+            <th>جواب</th>
+            <th>تعداد کلیک</th>
+            <th>عملیات</th>
+            <th></th>
+        </tr>
+        </thead>
         <?php
-        require_once "classes/UserGroup.php";
-
-        $userGroups = UserGroup::getAll();
-        foreach ($userGroups as $u) {
-            echo("<label for=\"" . $u->title . "\">" . $u->persionTitle . "</label><input type=\"checkbox\" id=\"" . $u->title . "\" name=\"" . $u->title . "\" value=\"" . $u->title . "\" checked>");
+        $faqs = FAQ::getAll(0);
+        foreach ($faqs as $f) {
+            echo("<tr>");
+            echo("<td>" . $f->id . "</td>");
+            echo("<td>" . $f->title . "</td>");
+            echo("<td>" . $f->answer . "</td>");
+            echo("<td>" . $f->click_count . "</td>");
+            echo("<td><a target=\"_blank\" href=\"AtachKeyword.php?faq=" . $f->id . "\">کلمات کلیدی</a></td>");
+            echo("<td><a target=\"_blank\" href=\"AttachPage.php?faq=" . $f->id . "\">صفحات</a></td>");
+            echo("</tr>");
         }
         ?>
-    </div>
-
-    <div>
-        <input type="submit" name="save" id="save" value="ذخیره">
-    </div>
-</form>
-
-<table style="width: 100%">
-    <tr>
-        <th>شناسه</th>
-        <th>سوال</th>
-        <th>جواب</th>
-        <th>تعداد کلیک</th>
-        <th>عملیات</th>
-    </tr>
-    <?php
-    $faqs = FAQ::getAll(0);
-    foreach ($faqs as $f) {
-        echo("<tr>");
-        echo("<td>" . $f->id . "</td>");
-        echo("<td>" . $f->title . "</td>");
-        echo("<td>" . $f->answer . "</td>");
-        echo("<td>" . $f->click_count . "</td>");
-        echo("<td><a target=\"_blank\" href=\"AtachKeyword.php?faq=" . $f->id . "\">کلمات کلیدی</a></td>");
-        echo("<td><a target=\"_blank\" href=\"AttachPage.php?faq=" . $f->id . "\">صفحات</a></td>");
-        echo("</tr>");
-    }
-    ?>
-</table>
+    </table>
 
 <?php
 
