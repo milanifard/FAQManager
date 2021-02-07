d189f54882961e6d5611a3f77ae7bd6eba19543b
MasoudK <masoud.kermanipoor@gmail.com>
2021-02-07 21:39:07 +0330

    fix save ticket

--- classes/Ticket.php
+++ classes/Ticket.php
@@ -20,6 +20,7 @@ class Ticket {
     static function save($title, $description, $userGroupId, $pageId) {
         $mysql = pdodb::getInstance();
         $mysql->Prepare("insert into tickets (title, description, project_id, tkt_bug_report_page, creator_type) values (?, ?, -1, ?, ?);");
+        $mysql->ExecuteStatement(array($title, $description, $pageId, $userGroupId));
     }
     
     static function getAllByContainsTerm($term){
